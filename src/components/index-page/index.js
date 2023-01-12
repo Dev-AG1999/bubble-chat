509fa4fdb84252fd848edf42107c38081725c788
Aishwariya Ghosal <aishwariya.ghosal04@gmail.com>
2023-01-12 21:26:06 +0530

    hello

--- src/components/index-page/index.js
+++ src/components/index-page/index.js
@@ -1,20 +1,42 @@
 
-import React from 'react'
+import React , { useState,useEffect }from 'react'
 import {Sidebar} from '../sidebar';
+import { auth } from "../../firebase";
+import { useNavigate } from 'react-router-dom';
 
 
 
 
-export const IndexPage = ({user}) => {
+export const IndexPage = () => {
+    const [User, setUser] = useState(null);
+    const history = useNavigate()
+
+    useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user has loggeed in
+
+        console.log(User);
+        setUser(authUser.displayName);
+      } else {
+        // the user has logged out
+
+        setUser(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [User]);
   return (
     
     <div className='home' >
-      
+      {User?(
       <div className="container" style={{display:"flex",width:"100%"}}>
     <Sidebar/>
 
  
-      </div>
+      </div>):(history("/login"))}
     </div>
   )
 }
