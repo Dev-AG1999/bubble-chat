f48bd3f0914c1bba062bc1c253008113b2a57856
Aishwariya Ghosal <aishwariya.ghosal04@gmail.com>
2023-01-16 16:15:42 +0530

    back camera

--- src/components/picture_capture.js
+++ src/components/picture_capture.js
@@ -1,35 +1,54 @@
 import React, { useState,useCallback,useRef } from 'react'
 import Webcam from 'react-webcam';
 import CameraInput from './input';
+
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
 const videoConstraints = {
  
-  facingMode: 'user',
+    facingMode: FACING_MODE_USER
 }
 export const OpenCamera= () => {
   const [picture, setPicture] = useState('');
   const webcamRef = useRef(null); 
+  const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
 
 
   const capture =useCallback(() => {
     const pictureSrc = webcamRef.current.getScreenshot()
-    setPicture(pictureSrc)
+    setPicture(pictureSrc);
+
   },[])
 
+  const handleClick = useCallback(() => {
+    setFacingMode(
+      prevState =>
+        prevState === FACING_MODE_USER
+          ? FACING_MODE_ENVIRONMENT
+          : FACING_MODE_USER
+    );
+  }, []);
 
   return (
     <div style={{width:"100vw",height:"100vh"}}>
        
-      <div style={{ height:"100%",width:"100%",position:"relative",display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <div style={{ height:"100%",width:"100%",position:"relative",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
 
         {picture ==='' ? (
+             <>
+                 
           <Webcam style={{height:"90%",width:"100%"}}
             audio={false}
             ref={webcamRef}
             screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
+            videoConstraints={{
+                ...videoConstraints,
+                facingMode
+              }}
           />
-        ) : (
-          <img alt='' style={{height:"90%",width:"100%",objectFit:"cover"}} src={picture} />
+            <button onClick={handleClick}>Switch camera</button>
+     </>  ) : (
+          <img alt='' style={{height:"100%",width:"100%",objectFit:"cover"}} src={picture} />
         )}
          <div style={{position:"absolute",bottom:"0px",width:"100%",display:"flex"}}>
         {picture !=='' ? (
